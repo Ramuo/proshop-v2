afce03f3ad4a5074715cee9704ad4f762c199e13
Barry <barry.alpha88@outlook.fr>
2023-05-05 15:37:00 +0200

    react router

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-    <footer>
+    <footer style={{backgroundColor: '#000'}}>
         <Container>
-            <Row>
+            <Row >
                 <Col className='text-center py-3'>
                     <p>ProShop &copy; {currentYear}</p>
                 </Col>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import {LinkContainer} from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
@@ -8,15 +9,25 @@ function Header() {
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>
-                    <img src={logo} alt="Proshop" />
-                    Proshop
-                </Navbar.Brand>
+                <LinkContainer to='/'>
+                    <Navbar.Brand >
+                        <img src={logo} alt="Proshop" />
+                        Proshop
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className="ms-auto">
-                        <Nav.Link href='/cart'><FaShoppingCart/>Panier</Nav.Link>
-                        <Nav.Link href='/login'><FaUser/>S'identifier</Nav.Link>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link>
+                                <FaShoppingCart/>Panier
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link>
+                                <FaUser/>S'identifier
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
