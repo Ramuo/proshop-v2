266185ac019e6ebcc872d86330832afba2622a13
Barry <barry.alpha88@outlook.fr>
2023-05-07 23:37:28 +0200

    controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,35 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
+
+
+
+//@desc     Fetch All products
+//@route    GET /api/products
+//@access   Public
+const getProducts = asyncHandler (async (req, res) => {
+    const products = await Product.find({});
+    res.json(products)
+});
+
+
+//@desc     Get Product By id
+//@route    GET /api/products/:id
+//@access   Public
+const getProductById = asyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if(product){
+        res.json(product);
+    }else{
+        res.status(404);
+        throw new Error('Produit Introuvable');
+    };
+    
+});
+
+
+
+export {
+    getProducts,
+    getProductById,
+}
