7dcaa43607d1a5d77e8267ca1fbb687f230f9f3e
Barry <barry.alpha88@outlook.fr>
2023-05-14 01:12:36 +0200

    User Routes & Controllers

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import {
+    loginUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    getUserById,
+    deleteUser,
+    updateUser
+} from '../controllers/userController.js'
+
+//Initialize router
+const router = express.Router();
+
+router.route('/')
+    .post(registerUser)
+    .get(getUsers);
+router.route('/logout').post(logoutUser);
+router.route('/login').post(loginUser);
+router.route('/profile')
+    .get(getUserProfile)
+    .put(updateUserProfile);
+router.route('/:id')
+    .delete(deleteUser)
+    .get(getUserById)
+    .put(updateUser)
+
+
+
+export default router
+
+
+/*
+import express from 'express';
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+
+
+const router = express.Router();
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router
+  .route('/profile')
+  .get(getUserProfile)
+  .put(updateUserProfile);
+router
+  .route('/:id')
+  .delete(deleteUser)
+  .get(getUserById)
+  .put(updateUser);
+
+export default router;
+*/
