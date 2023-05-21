1b23ce5572993f670bef9ae8beb2ff3040462842
Barry <barry.alpha88@outlook.fr>
2023-05-21 11:53:14 +0200

    Checkoutsteps

--- frontend/src/screens/ShippingAddress.jsx
+++ frontend/src/screens/ShippingAddress.jsx
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 
 import { saveShippingAddress } from '../slices/cartSlice';
@@ -19,9 +20,9 @@ const ShippingScreen = () => {
     
 
     const [address, setAddress] = useState(shippingAddress?.address || '');
-  const [city, setCity] = useState(shippingAddress?.city || '');
-  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
-  const [country, setCountry] = useState(shippingAddress?.country || '');
+    const [city, setCity] = useState(shippingAddress?.city || '');
+    const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+    const [country, setCountry] = useState(shippingAddress?.country || '');
     
 
 
@@ -39,7 +40,9 @@ const ShippingScreen = () => {
     return (
         <FormContainer>
         
-          <h1></h1>
+          <CheckoutSteps step1 step2/>
+          
+          <h1>Livraison</h1>
           <Form onSubmit={submitHandler}>
             <Form.Group className='my-2' controlId='address'>
               <Form.Label>Adresse</Form.Label>
