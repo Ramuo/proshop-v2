fb0717c9a67699c2f1b3606b004825f7ff500408
Barry <barry.alpha88@outlook.fr>
2023-06-03 16:43:06 +0200

    Backend User Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,28 +139,66 @@ const updateUserProfile = asyncHandler(async(req, res) => {
 //@route    GET /api/users
 //@access   Private/Admin
 const getUsers = asyncHandler(async(req, res) => {
-    res.send('Get users ');
+    const users = await User.find({});
+    res.status(200).json(users);
 });
 
 //@desc     Get user by ID
 //@route    GET /api/users/:id
 //@access   Private/Admin
 const getUserById = asyncHandler(async(req, res) => {
-    res.send('Get user By Id ');
+    const user = await User.findById(req.params.id).select('-password');
+
+    if(user){
+        res.status(200).json(user);
+    }else{
+        res.status(404);
+        throw new Error("Utilisateur non trouvé");
+    }
 });
 
 //@desc     Delete users
 //@route    DELETE /api/users/:id
 //@access   Private/Admin
 const deleteUser = asyncHandler(async(req, res) => {
-    res.send('Delete user ');
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        if(user.isAdmin){
+            res.status(404);
+            throw new Error("Impossible de supprimer un amdministrateur")
+        }
+        await User.deleteOne({_id: user._id});
+        res.status(200).json({message: 'Utilisateur supprimé avec succès'});
+    }else{
+        res.status(404);
+        throw new Error('Utilisateur non trouvé');
+    }
 });
 
 //@desc     Update user 
 //@route    PUT /api/users/:id
 //@access   Private/admin
 const updateUser = asyncHandler(async(req, res) => {
-    res.send('Update user  ');
+   const user = await User.findById(req.params.id);
+
+   if(user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+    });
+   }else{
+    res.status(404);
+    throw new Error('Utilisateur non trouvé');
+   }
 });
 
 export {
